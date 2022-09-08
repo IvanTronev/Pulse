37e741bb1db85c9d4c32c2d6379d916b708cfea7
Ivan_Tronev <74rus_86@mail.ru>
2022-09-09 00:51:45 +0300

    js file completion

--- src/js/app.js
+++ src/js/app.js
@@ -82,5 +82,24 @@ $(document).ready(function(){
   validateForms('#consultation form');
   validateForms('#order form');
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99");
+		$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+		$('form').submit(function(e) {
+			e.preventDefault();
+
+			if(!$(this).validate()) {
+				return;
+			}
+
+			$.ajax ({
+				type: "POST",
+				url: "mailer/smart.php",
+				data: $(this).serialize()
+			}).done(function() {
+				$(this).find("input").value("");
+
+				$('form').trigger('reset');
+			});
+			return false;
+		});
   });
