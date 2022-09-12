ae63161d31cb5f2d397f483bf13774e885066967
Ivan_Tronev <i74rus86@yandex.ru>
2022-09-12 21:54:50 +0300

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -101,5 +101,25 @@ $(document).ready(function(){
           $('form').trigger('reset');
       });
       return false;
-  });
+    });
+
+    // pageup
+
+    $(window).scroll (function(){
+      if ($(this).scrollTop () > 1600){
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    
+    $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+  
+    new WOW().init();
+
+    });
   });
